4d3faaa38d4847aa16b6c0a82de1b86b73899d5d
Pellevoisin <bouret.benjamin@gmail.com>
2022-09-20 22:07:03 +0200

    added config and gitignore

---
+++ config.py
@@ -0,0 +1,17 @@
+config = {
+    'API_PREFIX' : "https://eu.api.blizzard.com",
+    'FINKLE_ID' : '4744',
+    'IRONFOE_ID' : '5265',
+    'SULFURON_ID' : "4464",
+    'DYNAMIC_CLASSIC_WOTLK_NAMESPACE' : "dynamic-classic-eu",
+    'STATIC_CLASSIC_WOTLK_NAMESPACE' : "static-classic-eu",
+    'DYNAMIC_CLASSIC_ERA_NAMESPACE' : "dynamic-classic1x-eu",
+    'STATIC_CLASSIC_ERA_NAMESPACE' : "static-classic1x-eu",
+    'HORDE_AUCTION_HOUSE_ID' : "6",
+    'SEARCHED_ITEMS' : [
+	    {"item_id":"2775", "item_name":"Minerai d'argent", "max_price":15},
+	    {"item_id":"4369", "item_name":"Tromblon mortel", "max_price":20},
+	    {"item_id":"22451", "item_name":"Air primordial", "max_price":14}
+    ],
+    'MAX_AUCTIONS_RETURNED' : 5
+}
--- getAuctionAlerts.py
+++ getAuctionAlerts.py
@@ -1,29 +1,29 @@
 import json
 import requests
 import collections
+import config, secrets
 from requests.auth import HTTPBasicAuth
 from discord import Webhook, SyncWebhook
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/909048587042844683/l40jqfXb9QKciiHm7iqVFTjggw0uSPbuVKh_A0azty3MxgppBIktCuOqABqGJamXpw1S"
-CLIENT_ID = "94b01e3ac4d54cfd8117d2f67b11bf63"
-CLIENT_SECRET = "j2ITUFfObTsv0AyUe4fSByeqLSSHNYIn"
-API_PREFIX = "https://eu.api.blizzard.com"
+secrets = secrets.secrets
+config = config.config
+
+WEBHOOK_URL = secrets['WEBHOOK_URL']
+CLIENT_ID = secrets['CLIENT_ID']
+CLIENT_SECRET = secrets['CLIENT_SECRET']
+API_PREFIX = config['API_PREFIX']
 #Following values are retrieved using Postman Requests
-FINKLE_ID = "4744"
-IRONFOE_ID = "5265"
-SULFURON_ID = "4464"
-DYNAMIC_CLASSIC_WOTLK_NAMESPACE = "dynamic-classic-eu"
-STATIC_CLASSIC_WOTLK_NAMESPACE = "static-classic-eu"
-DYNAMIC_CLASSIC_ERA_NAMESPACE = "dynamic-classic1x-eu"
-STATIC_CLASSIC_ERA_NAMESPACE = "static-classic1x-eu"
-HORDE_AUCTION_HOUSE_ID = "6"
+FINKLE_ID =  config['FINKLE_ID']
+IRONFOE_ID = config['IRONFOE_ID']
+SULFURON_ID = config['SULFURON_ID']
+DYNAMIC_CLASSIC_WOTLK_NAMESPACE = config['DYNAMIC_CLASSIC_WOTLK_NAMESPACE']
+STATIC_CLASSIC_WOTLK_NAMESPACE = config['STATIC_CLASSIC_WOTLK_NAMESPACE']
+DYNAMIC_CLASSIC_ERA_NAMESPACE = config['DYNAMIC_CLASSIC_ERA_NAMESPACE']
+STATIC_CLASSIC_ERA_NAMESPACE = config['STATIC_CLASSIC_ERA_NAMESPACE']
+HORDE_AUCTION_HOUSE_ID = config['HORDE_AUCTION_HOUSE_ID']
 ######################################################
-SEARCHED_ITEMS = [
-	{"item_id":"2775", "item_name":"Minerai d'argent", "max_price":15},
-	{"item_id":"4369", "item_name":"Tromblon mortel", "max_price":20},
-	{"item_id":"22451", "item_name":"Air primordial", "max_price":14}
-]
-MAX_AUCTIONS_RETURNED = 10
+SEARCHED_ITEMS = config['SEARCHED_ITEMS']
+MAX_AUCTIONS_RETURNED = config['MAX_AUCTIONS_RETURNED']
 
 def setBoldDiscord(message):
 	boldMessage = "***"+message+"***"
